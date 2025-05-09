e83daed91a6f8ee917c579363dc183be1e818c1a
Max Novak <maxim.novak@gmail.com>
2025-05-09 10:24:47 +0100

    Move the creation of the vector extension to an alembic migration

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Initialize database tables using the correct path
-PYTHONPATH=/app/src python /app/src/init_db.py
-
 # Stamp the migrations as complete since tables are created
 alembic stamp head
 
