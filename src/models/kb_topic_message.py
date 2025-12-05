4ff8418541a9acd14fa369719bafd05ddfaff5b2
ilanbenb <12747039+ilanbenb@users.noreply.github.com>
2025-12-05 17:22:37 +0200

    bugfix - fix load (#46)

--- src/models/kb_topic_message.py
+++ src/models/kb_topic_message.py
@@ -2,5 +2,6 @@ from sqlmodel import Field, SQLModel
 
 
 class KBTopicMessage(SQLModel, table=True):
+    __tablename__ = "kb_topic_message"
     kb_topic_id: str = Field(foreign_key="kbtopic.id", primary_key=True)
     message_id: str = Field(foreign_key="message.message_id", primary_key=True)
